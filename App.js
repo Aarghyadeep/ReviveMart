a81f0afd004f1d426c8fbcf7a2d4ba0f30e19c99
Aarghyadeep Das <aarghyario98@gmail.com>
2024-04-06 19:52:52 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'dodgerblue',
+    width: '100%',
+    height: '30%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
